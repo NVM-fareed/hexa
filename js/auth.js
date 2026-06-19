16f4bb8a60b3b717d73580c2aec3890436b7cace
fareed <160044656+NVM-fareed@users.noreply.github.com>
2026-06-19 02:22:45 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -92,8 +92,7 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-})
- document.getElementById('loginForm').addEventListener('submit', function(e) {
+    document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault(); // Stops the page from reloading
 
     // 1. Grab all the input values from the HTML form
@@ -126,6 +125,9 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     .catch(error => console.error('Error:', error));
 });
+
+})
+
 
 
 
