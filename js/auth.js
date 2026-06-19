6a0297ec9ce71d8ad26c4296f6805d1d6acdb1ef
fareed <160044656+NVM-fareed@users.noreply.github.com>
2026-06-19 01:05:08 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -95,7 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 
-   document.getElementById('signupForm').addEventListener('submit', function(e) {
+   document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault(); // Stops the page from reloading
 
     // 1. Grab all the input values from the HTML form
